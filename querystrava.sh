10ad3990e328226e7a4896449e6f5f58396a6705
Dave Hughes <dlh3@sfu.ca>
2018-10-25 13:47:33 -0700

    Dynamic favicon code and positioning improvements

--- querystrava.sh
+++ querystrava.sh
@@ -424,16 +424,13 @@ qs_build_segments_board_from_ids() {
 			function setCrownCount(count) {
 				document.title = count + ' CRs';
 
-				var canvas = document.createElement('canvas');
-				canvas.height = 84;
-				canvas.width = 64;
+				var counterOffsetX = (count < 10 ? 18 : (count < 100 ? 6 : -6));
 
-				var countOffset = (count < 10 ? 18 : (count < 100 ? 6 : -5));
-
+				var canvas = \$('<canvas height=\"84\" width=\"64\">')[0];
 				var ctx = canvas.getContext('2d');
 				ctx.font = '50px serif';
 				ctx.fillText('👑', 7, 44);
-				ctx.fillText(count, countOffset, 84);
+				ctx.fillText(count, counterOffsetX, 84);
 
 				\$('link[rel~=\"icon\"]')[0].href = canvas.toDataURL();
 			}
