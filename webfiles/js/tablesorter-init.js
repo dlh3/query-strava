cfc30d5c5368b27dc991915cd895ef14f42cee1d
Dave Hughes <dlh3@sfu.ca>
2018-10-31 00:35:02 -0700

    Segment Board: When sorting, keep collapsible/expansion rows attached to their parent row

--- webfiles/js/tablesorter-init.js
+++ webfiles/js/tablesorter-init.js
@@ -3,7 +3,18 @@ $.tablesorter.defaults.widgets = ['zebra', 'filter', 'resizable', 'stickyHeaders
 
 $(document).ready(() => {
 	$('.tablesorter#segmentBoard').tablesorter({ sortList: [[3,0],[4,1]] });
+
+	// Collapsible rows should never be filtered out
 	$('.tablesorter').bind('filterEnd', () => $('.collapsible').removeClass('filtered'));
+
+	// Keep collapsible rows with their parent when sorting
+	$('.tablesorter').bind('sortEnd', () => {
+		$('.collapsible:visible').each((i, e) => {
+			var collapsibleRowId = e.id;
+			var segmentRowId = collapsibleRowId.replace(/^[^-]*-/, 'segmentRow-');
+			$('#' + segmentRowId).after($('#' + collapsibleRowId));
+		});
+	});
 });
 
 
