f28bcf5e4b9e0970bbb4a33e93ad7a1c4c4a4898
Dave Hughes <dlh3@sfu.ca>
2018-11-02 00:09:18 -0700

    Segment Board: format segment effort dates and fix sorting

--- webfiles/js/segments-expansion.js
+++ webfiles/js/segments-expansion.js
@@ -10,6 +10,20 @@ function toggleSegmentIframe(segmentId) {
 	currentRow.after(expansionRow);
 	expansionRow.toggleClass('hidden');
 
-	effortsTable.tablesorter();
-	segmentIframe.attr('src', segmentIframe.data('src'));
+	if (!expansionRow.hasClass('initialized')) {
+		// initialize tablesorter
+		effortsTable.tablesorter();
+
+		// convert ISO8601 dates to nicer format; do this after initializing tablesort (so it can sort timestamps)
+		effortsTable.find('.isoDate').each((i, e) => {
+			var date = new Date(e.innerText * 1000);
+			e.innerText = date.toDateString() + ' @ ' + date.toLocaleTimeString();
+		});
+
+		// copy the src data attribute to set the iframe src (prevents loading all iframes on page load)
+		segmentIframe.attr('src', segmentIframe.data('src'));
+
+		// mark this expansion row as initialized
+		expansionRow.addClass('initialized');
+	}
 }
